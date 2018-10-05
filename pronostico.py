df672c7abc5fe859c951ba5b2b671255bf7262e2
Carlos <carlosmaccarrone@gmail.com>
2018-10-05 16:12:50 -0300

    API - Temperatura actual en la ciudad

---
+++ pronostico.py
@@ -0,0 +1,13 @@
+## -*- coding: utf-8 -*-
+#Pronostico del tiempo en San Andrés, Buenos Aires
+#Carlos Esteban Maccarrone -cem- 2018
+ 
+import requests
+import json
+
+r = requests.get('https://ws.smn.gob.ar/map_items/weather')
+r.status_code
+
+datos = json.loads(r.text)
+
+print datos[35]['weather']['tempDesc']
