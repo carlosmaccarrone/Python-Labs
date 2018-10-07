2348d47ce4448ec8720479fbcb25fb835f679a9c
Carlos <carlosmaccarrone@gmail.com>
2018-10-07 17:58:37 -0300

    Googlear desde terminal con threads mas resultados

---
+++ garfios/googlearThreads.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+## -*- coding: latin-1 -*-
+
+import threading, time, httplib2
+from bs4 import BeautifulSoup
+
+http = httplib2.Http()
+cabecera = {'User-Agent': 'Mozilla/5.0'}
+
+url = raw_input('Googlear: ')
+print '\n'
+url = url.replace(' ','+')
+url = 'https://www.google.com/search?q=' + url
+
+respuesta = http.request(url, headers=cabecera )
+
+def busqueda(contador):
+	indice = contador - 1
+
+	if(contador == 0):
+		print respuesta[0].status
+		for web in BeautifulSoup(respuesta[1], 'lxml').find_all('h3', class_='r'):
+			try:
+				if( '/search?q=' in web.find('a')['href']):
+					print "Pagina 1"
+					print web.find('a')['href'].replace('/search?q=', 'https://www.google.com/search?q=') +'\n'
+				else:
+					print "Pagina 1"
+					print web.find('a')['href'].replace('/url?q=', '') +'\n'
+			except AttributeError, KeyError:
+				pass
+
+	if(contador == 1):
+		for web in BeautifulSoup(respuesta[1], 'lxml').find_all('div', {'id' : 'foot'}):
+			try:
+				paginaDos = http.request( 'https://www.google.com' + web.find_all('a', class_='fl')[indice]['href'], headers=cabecera )
+				print paginaDos[0].status	
+				for web in BeautifulSoup(paginaDos[1], 'lxml').find_all('h3', class_='r'):
+					try:
+						print "Pagina 2"
+						print web.find('a')['href'].replace('/url?q=', '') +'\n'
+					except AttributeError, KeyError:
+						pass
+			except AttributeError, KeyError:
+				pass
+
+	if(contador == 2):
+		for web in BeautifulSoup(respuesta[1], 'lxml').find_all('div', {'id' : 'foot'}):
+			try:
+				paginaTres = http.request( 'https://www.google.com' + web.find_all('a', class_='fl')[indice]['href'], headers=cabecera )
+				print paginaTres[0].status
+				for web in BeautifulSoup(paginaTres[1], 'lxml').find_all('h3', class_='r'):
+					try:
+						print "Pagina 3"
+						print web.find('a')['href'].replace('/url?q=', '') +'\n'
+					except AttributeError, KeyError:
+						pass
+			except AttributeError, KeyError:
+				pass
+
+threads = list()
+for i in range(3):
+    t = threading.Thread(target=busqueda, args=(i,))
+    threads.append(t)
+    t.start()
+    time.sleep(1)
+
+
+
+# Éste programa buscará en google, y mostrará
+# las primeras tres páginas de resultados
+
+# Para acceder a los links desde él terminal
+# mantenga pulsado Ctrl y haga click sobre el link 
+
+# pip install httplib2 beautifulsoup4
