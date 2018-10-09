ef4d8dbc464e12f35a77e06370b915f455d325d7
Carlos <carlosmaccarrone@gmail.com>
2018-10-09 15:15:43 -0300

    Crear servidor y sacar fotos (espía)

---
+++ garfios/cincoHTTPFotos.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2, time, threading, base64
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+def webcam():
+	for i in range(5):
+		rc, imagen = cap.read()
+		color = cv2.cvtColor(imagen, cv2.COLOR_BGR2BGRA)
+		capturas.append(color)
+		time.sleep(1)
+	cap.release()
+
+class vehiculo(BaseHTTPRequestHandler):
+    def do_GET(self):
+		for foto in capturas:
+			self.send_response(200)
+			self.end_headers()
+			rec, arrayIMG = cv2.imencode('.png', foto)
+			foto = base64.b64encode(bytearray(arrayIMG))
+			self.wfile.write('<img src="data:image/png;base64,'+foto+'"><br><br>')
+			self.wfile.write('\r\n')
+
+if __name__ == '__main__':
+	cap = cv2.VideoCapture(0)
+	global capturas
+	capturas = []
+	threads = list()
+	t = threading.Thread(target=webcam)
+	threads.append(t)
+	t.start()
+
+	urlSite = 'localhost' or '127.0.0.1'
+
+	try:
+		server = HTTPServer((urlSite, 8080), vehiculo)
+		print 'Starting server, use <Ctrl-C> to stop'
+		server.serve_forever()
+	except KeyboardInterrupt:
+		server.socket.close()
