9724c283af68456ffc5efa8866bf2cbc70086eb9
Carlos <carlosmaccarrone@gmail.com>
2018-10-16 00:58:14 -0300

    Googlear desde terminal con threads mas resultados

--- garfios/googlearThreads.py
+++ garfios/googlearThreads.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 ## -*- coding: latin-1 -*-
 
-import threading, time, httplib2
+import threading, httplib2
 from bs4 import BeautifulSoup
 
 http = httplib2.Http()
@@ -14,7 +14,7 @@ url = 'https://www.google.com/search?q=' + url
 
 respuesta = http.request(url, headers=cabecera )
 
-def busqueda(contador):
+def busqueda(contador, semaforo):
 	indice = contador - 1
 
 	if(contador == 0):
@@ -34,7 +34,7 @@ def busqueda(contador):
 		for web in BeautifulSoup(respuesta[1], 'lxml').find_all('div', {'id' : 'foot'}):
 			try:
 				paginaDos = http.request( 'https://www.google.com' + web.find_all('a', class_='fl')[indice]['href'], headers=cabecera )
-				print paginaDos[0].status	
+				print paginaDos[0].status
 				for web in BeautifulSoup(paginaDos[1], 'lxml').find_all('h3', class_='r'):
 					try:
 						print "Pagina 2"
@@ -58,18 +58,22 @@ def busqueda(contador):
 			except AttributeError, KeyError:
 				pass
 
+	semaforo.release()
+
 threads = list()
+sem = threading.BoundedSemaphore(value=1)
 for i in range(3):
-    t = threading.Thread(target=busqueda, args=(i,))
-    threads.append(t)
-    t.start()
-    time.sleep(1)
+	t = threading.Thread(target=busqueda, args=(i, sem))
+	threads.append(t)
+	sem.acquire()
+	t.start()
+
 
 
 # Éste programa buscará en google, y mostrará
 # las primeras tres páginas de resultados
 
 # Para acceder a los links desde él terminal
-# mantenga pulsado Ctrl y haga click sobre el link 
+# mantenga pulsado Ctrl y haga click sobre el link
 
 # pip install httplib2 beautifulsoup4
