2ccb1c134defed0be084bd32eda014659e028ef7
Carlos <carlosmaccarrone@gmail.com>
2019-01-25 15:02:25 -0300

    Muestra como creo base de datos en SQLite y hago c

--- sql-test/sqltest.py
+++ sql-test/sqltest.py
@@ -4,13 +4,13 @@ db = sqlite3.connect('sqltest.db')
 ptr = db.cursor()
 QUERY="INSERT INTO test VALUES (NULL, "
 
-print("Ingrese nombre")
+print 'Ingrese nombre'
 a = raw_input()
-print("Ingrese DNI")
+print 'Ingrese DNI'
 b = raw_input()
 
 ptr.execute(QUERY + "'" + a + "', " + b + ")")
-print(QUERY + "'" + a + "', " + b + ")")
+print str(QUERY) + "'" + a + "', " + b + ")"
 
 db.commit()
 db.close()
