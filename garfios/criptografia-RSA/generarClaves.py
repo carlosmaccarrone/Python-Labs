5312274a18a145925b8e8d9d0093b3cd393e07f2
Carlos <carlosmaccarrone@gmail.com>
2023-07-18 15:34:39 -0300

    cambio a python3

--- garfios/criptografia-RSA/generarClaves.py
+++ garfios/criptografia-RSA/generarClaves.py
@@ -5,8 +5,8 @@ from Crypto.PublicKey import RSA
 #Generar un par de claves Pública/Privada usando 4096 bits de longitud de clave (512 bytes)
 claveNueva = RSA.generate(4096, e=65537)
 
-clavePrivada = claveNueva.exportKey("PEM")
-clavePublica = claveNueva.publickey().exportKey("PEM")
+clavePrivada = claveNueva.exportKey()
+clavePublica = claveNueva.publickey().exportKey()
 
 print(clavePrivada)
 archivo = open("ejemplo/clavePrivada.pem", "wb")
@@ -17,3 +17,6 @@ print(clavePublica)
 archivo = open("ejemplo/clavePublica.pem", "wb")
 archivo.write(clavePublica)
 archivo.close()
+
+# pip install pycryptodome
+# pueden guardarse los certificados con extensión .cer .crt .pem y leerlos, funciona de la misma manera para todos
