3ee2e59a64236a5f1b5debacb07c5d6a1cf79c6c
Carlos <carlosmaccarrone@gmail.com>
2025-08-29 19:48:00 -0300

    add split_into_batches function

--- pandas_examples/split_into_batches.py
+++ pandas_examples/split_into_batches.py
@@ -1,13 +1,10 @@
 
 # This function is a classic for handling large data in chunks without breaking up RAM.
 
-class Utils:
-
-	@classmethod
-	def split_into_batches(cls, iterable, batch_size):
-		length = len(iterable)
-		for ndx in range(0, length, batch_size):
-			yield iterable[ndx : min(ndx + batch_size, length)]
+def split_into_batches(iterable, batch_size):
+    length = len(iterable)
+    for ndx in range(0, length, batch_size):
+        yield iterable[ndx : min(ndx + batch_size, length)]
 
 if __name__ == "__main__":
     # Suppose we have 23 records (can be IDs, objects, etc.)
@@ -16,6 +13,6 @@ if __name__ == "__main__":
     print("Complete dataset:", data)
 
     # We want to process in batches of 5
-    for i, batch in enumerate(Utils.split_into_batches(data, 5), start=1):
+    for i, batch in enumerate(split_into_batches(data, 5), start=1):
         print(f"Batch {i}: {batch}")
 
